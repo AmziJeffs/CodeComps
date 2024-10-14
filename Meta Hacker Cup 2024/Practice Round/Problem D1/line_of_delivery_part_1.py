07a57195c3013f203004e525ec23fdc37b93ac25
AmziJeffs <amzijeffs0@gmail.com>
2024-10-14 12:39:11 -0400

    Clean up Problem D1 solution

--- Meta Hacker Cup 2024/Practice Round/Problem D1/line_of_delivery_part_1.py
+++ Meta Hacker Cup 2024/Practice Round/Problem D1/line_of_delivery_part_1.py
@@ -11,31 +11,32 @@ with open(input_file, "r") as f:
 		for _ in range(N):
 			stones.append(int(f.readline()))
 		cases.append([G, stones])
-		
+
 
 print("Evaluating cases")
 
 results = []
 
 for goal, stones in cases:
-	# The set of energies is the same as the set of positions where stones land
-	# So, start by computing the shortest distance from some stone to the goal.
+	# The set of energies is the same as the set of positions where stones land.
+	# To start, compute the minimum distance from any stone to the goal.
 	distances_to_goal = [abs(stone-goal) for stone in stones]
 	min_distance = min(distances_to_goal)
 
 	# Now we need to find the least-indexed stone that lands at this distance.
-	# We can just iterate through the stones, noting that the i-th stone will
+	# We can iterate through the stones, noting that the i-th stone will
 	# land in the place i-th from last in the sorted list of resting positions.
 	resting_places = sorted(stones, reverse = True)
 	for i in range(len(resting_places)):
-		if abs(resting_places[i]-goal) == min_distance:
+		if abs(resting_places[i] - goal) == min_distance:
 			results.append([i, min_distance])
 			break
 
+
 print("Saving results")
 
-with open('results.txt', 'w') as f:
+with open("results.txt", "w") as f:
 	for i, result in enumerate(results):
-		f.write(f'Case #{i+1}: {result[0]+1} {result[1]}\n') # Not zero-indexed!
+		f.write(f"Case #{i+1}: {result[0]+1} {result[1]}\n") # Not zero-indexed!
 
 print("Complete")
